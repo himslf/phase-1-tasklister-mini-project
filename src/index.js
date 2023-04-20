c898b3546b6a3fc6bd5b962a6a8647aef6e59564
JASON YOUNG <moorslf@gmail.com>
2023-04-20 00:44:35 -0400

    TASKLISTER Mini Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (q)=> {
+    q.preventDefault();
+    buildListItem(form.querySelector("#new-task-description").value);
+    form.reset();
+  });
 });
+
+function buildListItem(thing) {
+  let listItem = document.createElement('li');
+  let btn = document.createElement('button');
+
+  btn.addEventListener('click',deleteItem);
+  btn.textContent = 'DELETE';
+  listItem.textContent = `${thing} `;
+  listItem.appendChild(btn);
+  document.querySelector('#tasks').appendChild(listItem);
+};
+
+function deleteItem(q) {
+  q.target.parentNode.remove();
+};
+
+function clearEverything() {
+  let list = document.querySelector('ul');
+  list.removeChild(list.firstElementChild);
+};
